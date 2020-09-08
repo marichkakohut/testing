29eed19bcba88892a3243d51ab1b6d20e20e9fe7
Marichka <marichkakohut06@gmail.com>
2020-09-08 17:06:00 +0300

    new changes

--- src/mery/Main.java
+++ src/mery/Main.java
@@ -1,12 +1,16 @@
 package mery;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         double waight = 53.5;
         int age = 19;
         String surname = " Kohut";
-        System.out.println(" hello my name is Marichka.");
         System.out.println("Привіт я Марічка" + surname+" мені " + age +" років" +" моя вага " + waight);
+        System.out.println("press any kay..");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
     }
 }
