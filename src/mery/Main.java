afeda88555789f807564977c29364805605c87be
Marichka <marichkakohut06@gmail.com>
2020-09-08 16:44:08 +0300

    updated information

--- src/mery/Main.java
+++ src/mery/Main.java
@@ -3,7 +3,10 @@ package mery;
 public class Main {
 
     public static void main(String[] args) {
+        double waight = 53.5;
+        int age = 19;
+        String surname = " Kohut";
         System.out.println(" hello my name is Marichka.");
-
+        System.out.println("Привіт я Марічка" + surname+" мені " + age +" років" +" моя вага " + waight);
     }
 }
